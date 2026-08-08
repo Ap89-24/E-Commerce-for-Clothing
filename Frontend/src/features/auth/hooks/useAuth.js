e5d3ddd53ba69b50f1e13451ba79b285f287efdd
Ap89-24 <aman082199@gmail.com>
2026-08-08 15:39:39 +0530

    setup api layer for register

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { register } from "../services/auth.api.js";
+import { setUser, setError, setLoading } from "../state/auth.slice.js";
+import { useCallback } from "react";
+
+
+
+export const useAuthActions = () => { 
+    const dispatch = useDispatch();
+    const handleRegister = useCallback(async ({
+        email,
+        fullName,
+        password,
+        contact,
+        isSeller = false
+    }) => { 
+        try {
+            const data = await register({
+                email,
+                fullName,
+                password,
+                contact,
+                isSeller
+            });
+            dispatch(setUser(data.user));
+            dispatch(setError(null));
+        } catch (error) {
+            const message = error ? error.message : "Registration failed";
+            dispatch(setError(message));
+        } finally {
+            dispatch(setLoading(false));
+        }
+    } , [dispatch]);
+
+    return {
+        handleRegister,
+    }
+};
+
---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const authApiInstance = axios.create({
+    baseURL: "http:localhost:3000/api/auth",
+    withCredentials: true,
+})
+
+export const register = async ({
+    email,
+    fullName,
+    password,
+    contact,
+    isSeller
+}) => { 
+    try {
+        const response = await authApiInstance.post("/register", {
+            email,
+            fullName,
+            password,
+            contact,
+            isSeller
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error("Error in registration", error);
+    }
+};
