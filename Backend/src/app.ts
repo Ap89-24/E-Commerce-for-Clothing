f41adc4ad0a49892106dd65282f4b09076d9ca03
Ap89-24 <aman082199@gmail.com>
2026-08-09 22:30:39 +0530

    Setup oAuth routes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,19 +3,32 @@ import expess from "express";
 import morgan from "morgan";
 import authRouter from "./routes/auth.route.js";
 import cors from "cors";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { config } from "./types/config.js";
+
 
 
 const app = expess();
 app.use(expess.json());
 app.use(expess.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+app.use(passport.initialize());
+passport.use(new GoogleStrategy({
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret: config.GOOGLE_CLIENT_SECRET,
+  callbackURL: config.GOOGLE_CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+ }));
 
 app.use("/api/auth" , authRouter);
 
--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { validateLoginUser, validateRegisterUser } from "../validators/auth.validator.js";
 import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import passport from "passport";
 
 
 
 const authRouter = Router();
 
-/* 
+/**  
 @description -> Register a new user
 @route -> POST /api/auth/register
 @access -> Public
@@ -14,12 +15,26 @@ const authRouter = Router();
 authRouter.post("/register" , validateRegisterUser , registerUser);
 
 
-/* 
+/**  
 @description -> Login a  user
 @route -> GET /api/auth/login
 @access -> Public
  */
 authRouter.post("/login", validateLoginUser, loginUser);
+
+/**
+@description -> Redirect user to Google Login page
+@route -> GET /api/auth/google
+@access -> Public
+ */
+authRouter.get("/auth/google", passport.authenticate("google", { scope: ["profile" , "email"] }));
+
+/**
+@description -> Handle Google OAuth callback and log in the user
+@route -> GET /api/auth/google/callback
+@access -> Public
+ */
+authRouter.get("/auth/google/callback", passport.authenticate("google", { session: false }));
 
 
 export default authRouter;
